31ff8aba359113b9b3e3eaf21872c0ee670ff408
gitUserSM <sumana_manoj@yahoo.com>
2020-08-23 22:46:35 -0400

    Lab21_MovieRegistration

---
+++ MovieRegistration/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Confirmation(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            else
+            {
+                return View("Index", movie);
+            }
+            
+        }
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =1)]
+        public string Title { get; set; }
+        public string Genre { get; set; }        
+        public int Year { get; set; }
+        public List<string> Actors { get; set; }
+        public List<string> Directors { get; set; }
+    }
+}
